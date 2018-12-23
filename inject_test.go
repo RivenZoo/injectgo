ec9812813b75f8767ad7f9397f88ede10fa5cf13
zrw <zrw2010@gmail.com>
2018-12-23 18:58:28 +0800

    test: add test case

--- inject_test.go
+++ inject_test.go
@@ -1,7 +1,59 @@
 package injectgo_test
 
-import "testing"
+import (
+	"fmt"
+	"testing"
 
-func TestHelloWorld(t *testing.T) {
-	// t.Fatal("not implemented")
+	"github.com/RivenZoo/injectgo"
+	"github.com/stretchr/testify/assert"
+)
+
+type Person struct {
+	Name string
+}
+
+func (p Person) String() string {
+	return fmt.Sprintf("name:%s", p.Name)
+}
+
+func TestInjectFields(t *testing.T) {
+	c := injectgo.NewContainer()
+
+	type B struct {
+		Name string
+	}
+
+	type A struct {
+		B        *B           `inject:""`
+		Stringer fmt.Stringer `inject:""`
+	}
+
+	a := &A{}
+	b := &B{"b"}
+	c.Provide(a, b, &Person{})
+
+	assert.Panics(t, func() {
+		c.Provide(B{})
+	}, "should panic because type not match")
+
+	c.Populate(nil)
+	assert.Equal(t, b, a.B)
+	assert.NotEmpty(t, a.Stringer.String())
+
+	type NamedA struct {
+		B *B `inject:"NameB"`
+	}
+	c = injectgo.NewContainer()
+	na := &NamedA{}
+
+	c.Provide(na, b)
+	assert.Panics(t, func() {
+		c.Populate(nil)
+	}, "should panic because no named value provided")
+
+	c = injectgo.NewContainer()
+	c.Provide(na)
+	c.ProvideByName("NameB", b)
+	c.Populate(nil)
+	assert.Equal(t, b, na.B)
 }
