e787655f0bf3b7c543ecc99f51d98c6beb082f36
zrw <zrw2010@gmail.com>
2018-12-23 19:31:55 +0800

    test: add inject function test case

--- inject_test.go
+++ inject_test.go
@@ -2,6 +2,7 @@ package injectgo_test
 
 import (
 	"fmt"
+	"github.com/pkg/errors"
 	"testing"
 
 	"github.com/RivenZoo/injectgo"
@@ -40,6 +41,7 @@ func TestInjectFields(t *testing.T) {
 	assert.Equal(t, b, a.B)
 	assert.NotEmpty(t, a.Stringer.String())
 
+	/// test named inject
 	type NamedA struct {
 		B *B `inject:"NameB"`
 	}
@@ -56,4 +58,66 @@ func TestInjectFields(t *testing.T) {
 	c.ProvideByName("NameB", b)
 	c.Populate(nil)
 	assert.Equal(t, b, na.B)
+}
+
+func TestInjectFunctions(t *testing.T) {
+	c := injectgo.NewContainer()
+
+	type B struct {
+		Name string
+	}
+
+	type A struct {
+		B        *B           `inject:""`
+		Stringer fmt.Stringer `inject:""`
+	}
+
+	var a *A
+	var b *B
+
+	c.ProvideFunc(injectgo.InjectFunc{
+		Fn: func() (*B, error) {
+			b = &B{"generated b"}
+			return b, nil
+		},
+	}, injectgo.InjectFunc{
+		Fn: func() *A { a = &A{}; return a },
+	}, injectgo.InjectFunc{
+		Fn: func() *Person { return &Person{Name: "unnamed person"} },
+	})
+	c.Populate(nil)
+
+	assert.NotNil(t, a)
+	assert.NotNil(t, b)
+	assert.Equal(t, b, a.B)
+	assert.NotEmpty(t, a.Stringer.String())
+
+	/// test named function inject
+	type NamedA struct {
+		B *B `inject:"NameB"`
+	}
+
+	c = injectgo.NewContainer()
+	na := &NamedA{}
+	c.Provide(na)
+	c.ProvideFuncByName("NameB", injectgo.InjectFunc{
+		Fn: func() *B {
+			b = &B{"generated b"}
+			return b
+		},
+	})
+	c.Populate(nil)
+	assert.Equal(t, b, na.B)
+
+	/// test function return error
+	c = injectgo.NewContainer()
+	assert.Panics(t, func() {
+		c.ProvideFunc(injectgo.InjectFunc{
+			Fn: func() (*B, error) {
+				b = &B{"generated b"}
+				return b, errors.New("unknown error")
+			},
+		})
+		c.Populate(nil)
+	})
 }
